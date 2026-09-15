65ca519def363c276c197fcfe1178ad65309043e
João Pedro <joaopedrorodriguesdida@gmail.com>
2026-09-15 01:15:11 -0300

    chore: adicionar estrutura base do Spring Boot

---
+++ src/main/java/br/edu/nassau/api_cursos/Application.java
@@ -0,0 +1,13 @@
+package br.edu.nassau.api_cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
